1c20276214cf9bcf53739b928dae33518bef22d7
Lawrence Nderitu Keru <ndech90@gmail.com>
2016-06-07 17:22:36 +0300

    Background Image
    Added background image

--- hooks/footer-extras.php
+++ hooks/footer-extras.php
@@ -1,3 +1,32 @@
+<?php
+
+	$script_name = basename($_SERVER['PHP_SELF']);
+	if($script_name == 'index.php' && isset($_GET['signIn'])){
+		?>
+		<style>
+			body{
+				background: url("images/rentalsuccess.jpg") no-repeat fixed center center / cover;
+			}
+		</style>
+		
+	<!--	<div class="alert alert-primary" id="benefits">
+			Benefits of becoming a member:
+			<ul>
+				<li> You'll be awesome!
+				<li> We'll be richer!
+			</ul>
+		</div> 
+		
+		<script>
+			$j(function(){
+				$j('#benefits').appendTo('#login_splash');
+			})
+		</script>   -->
+		<?php
+	}
+
+?>
+
 <div class="navbar-fixed-bottom hidden-print alert alert-info">
 	<?php echo date('D, j M Y h:m:s a T'); ?>
 </div>
